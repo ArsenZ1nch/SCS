18e2d92729021db5e62522283d4fa9aa1609babf
Arseniy Zinchenko <70096155+ArsenZ1nch@users.noreply.github.com>
2021-10-11 17:16:17 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,108 @@
+import os
+import json
 import discord
 from discord.ext.commands import Bot
 from dpyConsole import Console
 from termcolor import colored
 
-
-bot = Bot(command_prefix="!", intents=discord.Intents.all())
+bot = Bot(command_prefix='!', intents=discord.Intents.all())
 console = Console(bot)
 
 
 @bot.event
 async def on_ready():
-    print("Ready")
+    guild = await bot.fetch_guild(896724049286295582)
+    members = await guild.fetch_members().flatten()
+    # print('Starting to check the databases')
+    #
+    # with open('databases/social_credit.json', 'r+') as f:
+    #     jsonObj = json.load(f)
+    #     for member in members:
+    #         if str(member.id) not in jsonObj.keys():
+    #             print(colored(f'New member detected: {member}', 'yellow'))
+    #             jsonObj[str(member.id)] = 1000
+    #     f.seek(0)
+    #     json.dump(jsonObj, f, indent=4)
+    #
+    # print(colored('Databases synced up and ready to go', 'green'))
+    #
+    # for file in os.listdir('Cogs'):
+    #     if not file.endswith('.py'):
+    #         continue
+    #
+    #     try:
+    #         bot.load_extension(f'Cogs.{file[:-3]}')
+    #         print(colored(f'Loaded {file}', 'green'))
+    #     except Exception as E:
+    #         print(colored(f'Failed to Load {file}: {E}', 'red'))
+    # print('Finished')
+
+    print('Bot ready')
+    console.start()
 
 
 @bot.event
 async def on_disconnect():
-    print(colored("Disconnected", "red"))
+    print(colored('Disconnected', 'red'))
 
     @bot.event
     async def on_connect():
-        print(colored("Reconnected", "green"))
+        print(colored('Reconnected', 'green'))
 
 
 @console.command()
 async def dm(user: discord.User, msg):
-    print(f"Sending message to {user.name}: '{msg}'")
     await user.send(f"{msg}")
+    print(f"Sent message to {user.name}: '{msg}'")
+
+
+@console.command()
+async def loadCogs(*args):
+    if args[0] == '-A':
+        for file in os.listdir('Cogs'):
+            if not file.endswith('.py'):
+                continue
+
+            try:
+                bot.load_extension(f'Cogs.{file[:-3]}')
+                print(colored(f'Loaded {file}', 'green'))
+            except Exception as E:
+                print(colored(f'Failed to load {file}: {E}', 'red'))
+        return print('Finished')
+
+    for file in args:
+        try:
+            bot.load_extension(f'Cogs.{file}')
+            print(colored(f'Loaded {file}', 'green'))
+        except Exception as E:
+            print(E)
+            print(colored(f'Failed to load {file}: {E}', 'red'))
+    return print('Finished')
+
+
+@console.command()
+async def unloadCogs(*args):
+    if args[0] == '-A':
+        for file in os.listdir('Cogs'):
+            if not file.endswith('.py'):
+                continue
+
+            try:
+                bot.unload_extension(f'Cogs.{file[:-3]}')
+                print(colored(f'Unloaded {file}', 'green'))
+            except Exception as E:
+                print(colored(f'Failed to unload {file}: {E}', 'red'))
+        return print('Finished')
+
+    for file in args:
+        try:
+            bot.unload_extension(f'Cogs.{file}')
+            print(colored(f'Unloaded {file}', 'green'))
+        except Exception as E:
+            print(colored(f'Failed to unload {file}: {E}', 'red'))
+    return print('Finished')
 
 
 if __name__ == '__main__':
-    console.start()
-    bot.run("ODk2NzI3MTQ2MjQyOTIwNDk4.YWLUPA.Z4Cq4aOhjBC2RMoh-EWzYajhbf4")
+    bot.run(input('Enter the token: '))
+    # bot.run('')
---
+++ manual_cmds.py
@@ -0,0 +1,55 @@
+import discord
+from discord.ext import commands
+import json
+import time
+
+DATABASES = 'databases'
+SC_MAP = 'social_credit.json'
+LOGS = 'logs.json'
+
+
+class ManCmds(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def promote(self, ctx, usr: discord.Member, sc: int, *, reason=None):
+        if ctx.author.top_role < ctx.guild.get_role(763448121786368060):
+            return await ctx.send('不')
+
+        if ctx.author.top_role <= usr.top_role or sc == 0:
+            return await ctx.send('不')
+
+        if sc > 0:
+            change = 'Promotion'
+        else:
+            change = 'Demotion'
+
+        with open(f'{DATABASES}/{SC_MAP}', 'r+') as f:
+            jsonObj = json.load(f)
+            jsonObj[str(usr.id)] += sc
+            f.seek(0)
+            json.dump(jsonObj, f, indent=4)
+
+        with open(f'{DATABASES}/{LOGS}', 'r+') as f:
+            jsonObj = json.load(f)
+
+            epoch = str(int(time.time()))
+            jsonObj.update({
+                str(usr.id):
+                    {epoch: {
+                        'Change': change,
+                        'Change of social credit': sc
+                    }}
+            })
+            if reason:
+                jsonObj[str(usr.id)][epoch].update({'Reason': reason})
+
+            f.seek(0)
+            json.dump(jsonObj, f, indent=4)
+
+        print('finished')
+
+
+def setup(client):
+    client.add_cog(ManCmds(client))
